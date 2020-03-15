a5a6169ee40fdbfab50108b4e6ab107342ad2cc9
Tassilo <38081490+TASSIA710@users.noreply.github.com>
2020-03-15 13:43:27 +0100

    Create endroundmusic_songs.lua

---
+++ lua/autorun/endroundmusic_songs.lua
@@ -0,0 +1,19 @@
+
+--[[
+	TTT End Round Music -> Songs (ServerSide)
+	by Tassilo (https://github.com/TASSIA710)
+--]]
+
+TassiloEndRoundMusic.Songs = {
+	-- Add your songs, use the following format
+  --  {
+  --    Author = "ABBA",
+  --    Title = "Dancing Queen",
+  --    URL = "http://yourwebserver.xyz/path/to/sounds/ABBA%20-%20Dancing%20Queen.mp3"
+  --  },
+  --  {
+  --    Author = "The Fratellis",
+  --    Title = "Chelsea Dagger",
+  --    URL = "http://yourwebserver.xyz/path/to/sounds/The%20Fratellis%20-%20Chelsea%20Dagger"
+  --  }
+}
